25b07f5a83e5451165e2eb02cb1ec29200f27d00
joshua <joshvigel329@outlook.com>
2023-12-13 09:54:55 -0600

    changes

---
+++ sana/screens/Contacts.js
@@ -0,0 +1,300 @@
+import React, {useState, useEffect} from "react";
+import {StyleSheet, Text, View, Image, TouchableOpacity, FlatList} from "react-native";
+import {auth, database} from '../config/firebase';
+import {collection, doc, query, onSnapshot, addDoc, where} from 'firebase/firestore';
+import { Dimensions } from 'react-native'
+import Verification from "./Verification.js"
+
+const vh = Dimensions.get('window').height / 100;
+const vw = Dimensions.get('window').width / 100;
+
+class ContactClass {
+    constructor (cid, username) {
+        this.cid = cid;
+        this.username = username;
+    }
+}
+
+const addPerson = () => {
+    const newConvo = addDoc(collection(database, "chat"), {
+        uids:[auth?.currentUser?.uid, "HltL8GyfH7enbscAfsqtYyUDQ1A2"],
+        groupName: "test", 
+    })
+    console.log("e  " + newConvo.id);
+}
+
+export default function Contact({navigation}) {
+
+    const [contacts, setContacts] = useState([]);
+    const [cids, setCids] = useState([]);
+
+  //useEffect(() => {
+  //  if (!auth?.currentUser?.emailVerified) {
+  //    navigation.navigate("Verification")
+  //  }
+  //});
+  
+  useEffect(() => {
+    const unsubscribe = onSnapshot(doc(database, "users", auth?.currentUser?.uid), { includeMetadataChanges: true }, (doc) => {
+        setCids(doc.data.cids);
+    })
+    return () => unsubscribe();
+  }, []);
+  
+  useEffect(() => {
+    const chatRef = collection(database, "chat")
+    const q = query(chatRef, where("uids", "array-contains", auth?.currentUser?.uid));
+ 
+      const unsubscribe = onSnapshot(q, 
+    { includeMetadataChanges: true },
+    (querySnapshot) => {
+    //console.log(querySnapshot.docs);
+    setContacts([]);
+    querySnapshot.forEach((doc) => {
+        const tempContact = new ContactClass(doc.id, doc.data().groupName);
+        //const tempContacts = [...contacts, tempContact];
+        setContacts(prevContacts => ([...prevContacts, tempContact]));
+      })
+    })
+    return () => unsubscribe();
+  }, []);
+
+  return (
+        <View style={[styles.container, {flexDirection:"column"}]}>
+
+            <View style={styles.header}>
+                <View style={{flexDirection:"row", display: "flex", alignItems: "center", width: "100vw", justifyContent: "space-evenly"}}>
+                    
+                    
+                    <Image style={styles.headerLogo} source={require('../images/sana-logo-2.png')}/>
+                    
+                    <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+                        <Image style={styles.headerLogo} source={require('../images/settings.png')}/>
+                    </TouchableOpacity>
+                    
+                </View>
+            </View>
+
+            <View style={styles.bottom}>
+                <View style={styles.contactList}>
+                    <TouchableOpacity style={styles.item} onPress={() => addPerson()}>
+                      <View style={styles.itemTextBox}>
+                      <Text style={styles.itemText} >{"Add New Friend"}</Text>
+                      </View>
+                    </TouchableOpacity>
+                    
+                    <TouchableOpacity style={[styles.item, {backgroundColor: "rgba(0,0,0,0.2)"}]} onPress={() => navigation.navigate('Chat', {username: "Global Chat", cid: "hello"})}>
+                        <Text>{"Global Chat"}</Text>
+                    </TouchableOpacity>
+                
+                    <FlatList
+                      keyExtractor={(item)=>item.cid}
+                      data={contacts}
+                      renderItem={({item}) => (
+                        <View>
+                            <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('Chat', {userName: item.username, cid: item.cid})}>
+                                <Text>{item.username}</Text>
+                            </TouchableOpacity>
+                        </View>
+                      )}
+                    />
+               
+                </View>
+            </View>
+      </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+
+    header: {
+        flex:2,
+        width: "100vw",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        
+    },
+    bigText:{
+        fontSize: 100,
+        fontFamily: "Futara",
+        fontWeight: "bold",
+    },
+    smallBigText:{
+        fontSize: 40,
+        fontFamily: "Futara",
+    },
+    headerLogo: {
+      width: 40,
+      height: 40,
+      resizeMode: "contain",
+    },
+    bottom: {
+        flex: 8,
+        flexDirection: "row",
+        width: "100vw",
+        height: "80vh",
+        borderRadius:9,
+    },
+    contactList: {
+      flex:1,
+      backgroundColor: "white",
+    },
+    homePart: {
+      flex:3,
+      backgroundColor: "white",
+    },
+    container: {
+        flex:1,
+        alignItems: 'center', 
+        justifyContent: 'center',
+        backgroundColor: "white"
+    },
+    title: {
+        fontSize:36,
+        fontWeight: 'bold',
+        color: "orange",
+    },
+    input: {
+        backgroundColor: "red",
+        height: 58,
+        marginBottom: 20,
+        fontSize: 16,
+        borderRadius: 10,
+        padding: 12,
+    },
+    form: {
+        flex:1,
+        justifyContent: 'center',
+        marginHorizontal: 30,
+    },
+    card: {
+        backgroundColor: "blue",
+        height: 58,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    buttonText: {
+        fontWeight: "bold",
+        color: "black",
+        fontSize: 18,
+    },
+    button: {
+        backgroundColor: "blue",
+        height: 58,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+  item: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 15,
+    width: 90 * vw,
+    height: 10 * vh,
+    marginHorizontal: 5 * vw,
+  },
+});
+/*
+
+        <View style={styles.header}>
+          <View style={{flexDirection:"row"}}>
+            <View style={{flex:0.5}}/>
+            <Image style={styles.headerLogo} source={require('../images/sana-logo-2.png')}/>
+            <View style={{flex:6}}/>
+            <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+            <Image style={styles.headerLogo} source={require('../images/settings.png')}/>
+            </TouchableOpacity>
+            <View style={{flex:0.5}}/>
+            <TouchableOpacity onPress={() => auth.signOut()}>
+            <Image style={styles.headerLogo} source={require('../images/default.svg') }/>
+            </TouchableOpacity>
+          </View>
+        </View>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Chat', {userName: "Global Chat", cid: "OgrB6M9OcEkMLLoyqbQ5"})}>
+                <Text>{"Global Chat"}</Text>
+            </TouchableOpacity>
+            </View>
+            <View style={styles.homePart}>
+            <Text style={styles.bigText}> Welcome! </Text>
+            <Text style={styles.smallBigText}> Select a chat or icon to get started. </Text>
+            </View>
+            */
+/*
+
+                    <FlatList 
+          data={contacts}
+          keyExtractor={item=>item.cid}
+          renderItem={({item}) => (
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Chat', {userName: item.username, cid: item.cid})}>
+                <Text>{item.username}</Text>
+            </TouchableOpacity>
+          )}
+          */
+ /*
+    var [contacts, setContacts] = useState([]);
+  var [contact, setContact] = useState([]);
+  var [usernames, setUsernames] = useState([]);
+  const 
+  const user = auth.currentUser.uid;
+  const docRef = doc(database, "users", user);
+  getDoc(docRef).then(docSnap => {
+    var data;
+  if (docSnap.exists()) {
+    data = docSnap.data();
+  }
+  var cids = data != null ? data.cids : [];
+      for (let cid of cids) {
+        const chatRef = doc(database, "chat", cid);
+        getDoc(chatRef).then(chatSnap => {
+          var uids;
+        if (chatSnap.exists()) {
+            const chatData = chatSnap.data();
+            uids = chatData.uids;
+            var index = uids.indexOf(user);
+            if (index !== -1) {
+              uids.splice(index, 1);
+            }
+            //contacts.push(tempContact);
+        }
+        for  (let uid of uids){
+          const userRef = doc(database, "users", uid)
+          getDoc(userRef).then(userSnap => {
+            var username;
+            if(userSnap.exists()) {
+              const userData = userSnap.data();
+              username = userData.fName + " " + userData.lName
+            }
+            setUsernames(username);
+            //console.log(usernames)
+          })
+        }
+        //var userNames = usernames.toString();
+        const tempContact = new ContactClass(cid, "Neeraj Gogate");
+        setContact(tempContact);
+        setContacts(contact);
+      })
+      }
+    }
+  )
+  */
+
+
+  /*
+
+        const unsub = onSnapshot(cidRef, 
+    { includeMetadataChanges: true },
+    (chatDoc) => {
+          const uids = chatDoc.data().uids;
+          for (const uid of uids) {
+            console.log(uid);
+          }
+        })
+        unsub();
+        */
